66f1676b834d5690935b521b0ec918d61c214bfe
Phil Walton <philipwalton123@googlemail.com>
2023-03-21 15:08:56 +0000

    add quotes widget

--- microservice-app/widget_1/public/script.js
+++ microservice-app/widget_1/public/script.js
@@ -1,1 +1,14 @@
-console.log("widget one script loaded")
+// Define an array of inspirational quotes
+const quotes = [
+    "Believe you can and you're halfway there. -Theodore Roosevelt",
+    "Strive not to be a success, but rather to be of value. -Albert Einstein",
+    "Success is not final, failure is not fatal: It is the courage to continue that counts. -Winston S. Churchill",
+    "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle. -Christian D. Larson",
+    "Don't watch the clock; do what it does. Keep going. -Sam Levenson"
+];
+
+// Generate a random index to select a quote from the array
+const index = Math.floor(Math.random() * quotes.length);
+
+// Display the randomly selected quote
+document.getElementById("quote").innerHTML = quotes[index];
