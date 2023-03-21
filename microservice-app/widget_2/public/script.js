49fc5c6f5e4c80d643cf838c7831b6f15465a8e7
Phil Walton <philipwalton123@googlemail.com>
2023-03-21 17:20:48 +0000

    add 8-ball widget

--- microservice-app/widget_2/public/script.js
+++ microservice-app/widget_2/public/script.js
@@ -1,1 +1,24 @@
-console.log("widget two script loaded")
+const answers = ["yes", "no", "that depends", "don't count on it", "outlook is not so good", "my sources say yes", "it's a certainty" , "you bet"]
+
+
+window.onload = function() {
+    const eight = document.getElementById("eight")
+    const answer = document.getElementById("answer")
+    const eightBall = document.getElementById("eight-ball")
+    const question = document.getElementById("question")
+
+    eightBall.addEventListener("click", function(event) {
+        console.log("click")
+        if (question.value.length < 1) {
+            alert("enter a question")
+        } else {
+            question.value = ""
+            eight.hidden = true
+            const num = Math.floor(Math.random() * Math.floor(answers.length))
+            const answerText = answers[num]
+            console.log(answerText)
+            answer.innerText = answerText
+        }        
+    })
+
+}
