1f265880c61e90749175dffa968941a30d20772a
Phil Walton <philipwalton123@googlemail.com>
2023-03-28 07:55:27 +0100

    add check for ends-with 'panda'

--- panda_name.sh
+++ panda_name.sh
@@ -6,6 +6,13 @@ if [[ $# -eq 0 ]] ; then
     exit 0
 fi
 
+if [[ $1 != *panda ]] ; then
+    echo "Check your panda name and try again"
+    echo "It must end with panda eg. "
+    echo "$(pwd)/panda_name.sh cool-panda"
+    exit 0
+fi
+
 if [[ $1 != *-* ]] ; then
    echo "ERROR: Check your panda name and try again"
    echo "It must contain a dash eg. "
@@ -27,7 +34,7 @@ files_with_pandaname=("microservice-app/nginx.conf" "microservice-app/html/scrip
 # done;
 
 for file in "${files_with_pandaname[@]}"; do 
-  cat ~/workdir/NGINX-Workshop/templates/$file | sed s:INSERT_PANDA_NAME:$1:g > ~/workdir/NGINX-Workshop/$file;
+  cat ~/playground/NGINX-Workshop/templates/$file | sed s:INSERT_PANDA_NAME:$1:g > ~/playground/NGINX-Workshop/$file;
 done;
 
 echo "Added: $1"
